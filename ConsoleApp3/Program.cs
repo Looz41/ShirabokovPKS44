f2774bd31935a906fa7b311c2a78a6ad37950266
Looz <Shirabokovi99@gmail.com>
2024-02-04 21:30:50 +0300

    correct

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,33 +4,21 @@ class Program
 {
     static void Main()
     {
-        double firstNumber, secondNumber;
-
-        Console.WriteLine("Введите первое число:");
-        if (!double.TryParse(Console.ReadLine(), out firstNumber))
+        Console.WriteLine("Введите число:");
+        if (int.TryParse(Console.ReadLine(), out int userInput))
         {
-            Console.WriteLine("Ошибка ввода первого числа.");
-            return;
-        }
-
-        Console.WriteLine("Введите второе число:");
-        if (!double.TryParse(Console.ReadLine(), out secondNumber))
-        {
-            Console.WriteLine("Ошибка ввода второго числа.");
-            return;
-        }
-
-        if (firstNumber == secondNumber)
-        {
-            Console.WriteLine("Введенные числа равны.");
-        }
-        else if (firstNumber > secondNumber)
-        {
-            Console.WriteLine("Первое число больше второго.");
+            if (userInput >= 5 && userInput <= 10)
+            {
+                Console.WriteLine("Число больше 5 и меньше 10.");
+            }
+            else
+            {
+                Console.WriteLine("Неизвестное число.");
+            }
         }
         else
         {
-            Console.WriteLine("Первое число меньше второго.");
+            Console.WriteLine("Ошибка ввода числа.");
         }
 
         Console.ReadLine();
