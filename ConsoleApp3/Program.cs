c4db22a85a1e9c6462f4aa59fd92a5ce72404e6a
Looz <Shirabokovi99@gmail.com>
2024-02-04 21:26:45 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double firstNumber, secondNumber;
+
+        Console.WriteLine("Введите первое число:");
+        if (!double.TryParse(Console.ReadLine(), out firstNumber))
+        {
+            Console.WriteLine("Ошибка ввода первого числа.");
+            return;
+        }
+
+        Console.WriteLine("Введите второе число:");
+        if (!double.TryParse(Console.ReadLine(), out secondNumber))
+        {
+            Console.WriteLine("Ошибка ввода второго числа.");
+            return;
+        }
+
+        if (firstNumber == secondNumber)
+        {
+            Console.WriteLine("Введенные числа равны.");
+        }
+        else if (firstNumber > secondNumber)
+        {
+            Console.WriteLine("Первое число больше второго.");
+        }
+        else
+        {
+            Console.WriteLine("Первое число меньше второго.");
+        }
+
+        Console.ReadLine();
+    }
+}
