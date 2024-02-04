ff4ecd0406df3cbf95fd56acd98b54d8006fc07f
Looz <Shirabokovi99@gmail.com>
2024-02-04 21:58:02 +0300

    correct

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,35 +4,29 @@ class Program
 {
     static void Main()
     {
-        double firstNumber, secondNumber;
+        while (true)
+        {
+            Console.Write("Введите первое число (от 0 до 10): ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Введите первое число:");
-        if (!double.TryParse(Console.ReadLine(), out firstNumber))
-        {
-            Console.WriteLine("Ошибка ввода первого числа.");
-            return;
-        }
+            Console.Write("Введите второе число (от 0 до 10): ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Введите второе число:");
-        if (!double.TryParse(Console.ReadLine(), out secondNumber))
-        {
-            Console.WriteLine("Ошибка ввода второго числа.");
-            return;
+            if (IsValidInput(number1) && IsValidInput(number2))
+            {
+                int result = number1 * number2;
+                Console.WriteLine($"Результат умножения: {result}");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Введенные числа недопустимы. Пожалуйста, введите числа от 0 до 10.");
+            }
         }
+    }
 
-        if (firstNumber == secondNumber)
-        {
-            Console.WriteLine("Введенные числа равны.");
-        }
-        else if (firstNumber > secondNumber)
-        {
-            Console.WriteLine("Первое число больше второго.");
-        }
-        else
-        {
-            Console.WriteLine("Первое число меньше второго.");
-        }
-
-        Console.ReadLine();
+    static bool IsValidInput(int number)
+    {
+        return number >= 0 && number <= 10;
     }
 }
