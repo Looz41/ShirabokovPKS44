6cb7c7055df0c75f7ccfaeac50bb245a921759de
Looz <Shirabokovi99@gmail.com>
2024-02-04 22:00:15 +0300

    correct

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,35 +4,42 @@ class Program
 {
     static void Main()
     {
-        double firstNumber, secondNumber;
+        // Ввод массива чисел от пользователя
+        Console.Write("Введите элементы массива через пробел: ");
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(' ');
 
-        Console.WriteLine("Введите первое число:");
-        if (!double.TryParse(Console.ReadLine(), out firstNumber))
+        // Обработка ввода и заполнение массива
+        int[] numbers = new int[inputArray.Length];
+        for (int i = 0; i < inputArray.Length; i++)
         {
-            Console.WriteLine("Ошибка ввода первого числа.");
-            return;
-        }
-
-        Console.WriteLine("Введите второе число:");
-        if (!double.TryParse(Console.ReadLine(), out secondNumber))
-        {
-            Console.WriteLine("Ошибка ввода второго числа.");
-            return;
-        }
-
-        if (firstNumber == secondNumber)
-        {
-            Console.WriteLine("Введенные числа равны.");
-        }
-        else if (firstNumber > secondNumber)
-        {
-            Console.WriteLine("Первое число больше второго.");
-        }
-        else
-        {
-            Console.WriteLine("Первое число меньше второго.");
+            if (int.TryParse(inputArray[i], out numbers[i]))
+            {
+                // Введенные данные успешно преобразованы в целое число
+            }
+            else
+            {
+                Console.WriteLine($"Ошибка ввода! Невозможно преобразовать '{inputArray[i]}' в целое число.");
+                return;
+            }
         }
 
-        Console.ReadLine();
+        Console.WriteLine("Нечетные числа из массива:");
+
+        // Вывод нечетных чисел
+        foreach (int number in numbers)
+        {
+            if (IsOdd(number))
+            {
+                Console.Write(number + " ");
+            }
+        }
+
+        Console.WriteLine();
+    }
+
+    static bool IsOdd(int number)
+    {
+        return number % 2 != 0;
     }
 }
