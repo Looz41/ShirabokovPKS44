f0123d592e5fe92e133d9926d743538bbb7c1320
Looz <Shirabokovi99@gmail.com>
2024-02-16 05:11:39 +0300

    correct

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,35 +4,44 @@ class Program
 {
     static void Main()
     {
-        double firstNumber, secondNumber;
+        Console.WriteLine("Введите элементы первого массива (через пробел):");
+        int[] array1 = ReadIntArrayFromConsole();
 
-        Console.WriteLine("Введите первое число:");
-        if (!double.TryParse(Console.ReadLine(), out firstNumber))
+        Console.WriteLine("Введите элементы второго массива (через пробел):");
+        int[] array2 = ReadIntArrayFromConsole();
+
+        int[] mergedArray = MergeArrays(array1, array2);
+
+        Console.WriteLine("Объединенный массив: [" + string.Join(", ", mergedArray) + "]");
+    }
+
+    static int[] ReadIntArrayFromConsole()
+    {
+        string input = Console.ReadLine();
+        string[] strArray = input.Split(' ');
+        int[] intArray = new int[strArray.Length];
+        for (int i = 0; i < strArray.Length; i++)
         {
-            Console.WriteLine("Ошибка ввода первого числа.");
-            return;
+            intArray[i] = int.Parse(strArray[i]);
+        }
+        return intArray;
+    }
+
+    static int[] MergeArrays(int[] arr1, int[] arr2)
+    {
+        int totalLength = arr1.Length + arr2.Length;
+        int[] mergedArray = new int[totalLength];
+
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            mergedArray[i] = arr1[i];
         }
 
-        Console.WriteLine("Введите второе число:");
-        if (!double.TryParse(Console.ReadLine(), out secondNumber))
+        for (int i = 0; i < arr2.Length; i++)
         {
-            Console.WriteLine("Ошибка ввода второго числа.");
-            return;
+            mergedArray[arr1.Length + i] = arr2[i];
         }
 
-        if (firstNumber == secondNumber)
-        {
-            Console.WriteLine("Введенные числа равны.");
-        }
-        else if (firstNumber > secondNumber)
-        {
-            Console.WriteLine("Первое число больше второго.");
-        }
-        else
-        {
-            Console.WriteLine("Первое число меньше второго.");
-        }
-
-        Console.ReadLine();
+        return mergedArray;
     }
 }
